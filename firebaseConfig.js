6cae857cec6e492a866a45411b7294bde8e79b27
dylann-s <157635473+dylann-s@users.noreply.github.com>
2024-04-20 04:51:10 -0400

    Add files via upload

---
+++ firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAyjtUDkdHKS9qHNb70Le5ebFyb7b1nTbQ",
+  authDomain: "info1601-5d056.firebaseapp.com",
+  projectId: "info1601-5d056",
+  storageBucket: "info1601-5d056.appspot.com",
+  messagingSenderId: "603045611166",
+  appId: "1:603045611166:web:a7841943214d38279c5d8f",
+  measurementId: "G-S1R23D13WZ"
+};
+
+export default firebaseConfig;
